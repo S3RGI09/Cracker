45380fbd58baef196ae8b3203e68a0385b270a17
Sergio Casero Verdial <96842235+S3RGI09@users.noreply.github.com>
2022-04-08 15:16:02 +0200

    Update cracker.py

--- cracker.py
+++ cracker.py
@@ -7,17 +7,17 @@ import hashlib
 print()
 print("  ____ ____      _    ____ _  _______ ____  ")
 print(" / ___|  _ \    / \  / ___| |/ / ____|  _ \ ")
-print("| |   | |_) |  / _ \| |   | ' /|  _| | |_) |")
+print("| |   | |_) |  / _ \| |   | ' /|  _| | |_) |   github.com/S3RGI09")
 print("| |___|  _ <  / ___ \ |___| . \| |___|  _ < ")
 print(" \____|_| \_\/_/   \_\____|_|\_\_____|_| \_\   V1.5")
 print()
-print("creado por S3RGI09 (Sergio Casero Verdial)")
+print("\033[;36m"+"Creado por S3RGI09 (Sergio Casero Verdial)")
 
 encontrada = 0
-print("-----------------------------------------------")
-input_hash = input("Inserte la contraseña hasheada: ")
-pass_doc = input("Inserte el diccionario: ")
-print("-----------------------------------------------")
+print("\033[1;31m"+"--------------------------------------------")
+input_hash = input("\033[1;32m"+"[+] Inserte la contraseña haseada MD5: "+'\033[0;m') 
+pass_doc = input("\033[1;32m"+"[+] Inserte el diccionario: "+'\033[0;m') 
+print("\033[1;31m"+"--------------------------------------------")
 
 try:
 	pass_file = open(pass_doc, 'r')
