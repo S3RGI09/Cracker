e6cb969dec838964deb43af7c1b18e95de6a1f38
Sergio Casero Verdial <96842235+S3RGI09@users.noreply.github.com>
2022-04-21 20:28:47 +0200

    Create instalacion.sh

---
+++ instalacion.sh
@@ -0,0 +1,8 @@
+echo "Bienvenido al asistente de instalacion de Cracker, vamos a empezar por instalar las librerias de python."
+pip install hashlib
+echo "Si te da error significa que ya las tienes instaladas o que tu version de python no es la adecuada, recuerda, Python3"
+echo "ahora vamos a darle permisos de ejecucion al script, esto SOLO FUNCIONA EN SISTEMAS UNIX-LIKE"
+chmod +x cracker.py
+echo "Ahora solo pon en tu consola "./cracker.py" ya tienes el script listo"
+echo "Gracias por instalar Cracker, ahora vamos a borrar este instalador"
+rm instalacion.sh
